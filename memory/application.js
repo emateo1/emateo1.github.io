ad7a89cd441e9f5bb317e07f44aafae9c4eec9ff
emateo1 <emateo1@kippnyccp.org>
2015-03-05 13:25:31 -0500

    Update and rename memory to memory/application.js

---
+++ memory/application.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+  for(i=1; i<21; i++){
+    $('#card_holder').append('<div class="card"><p>'+i+'</p></div>');
+  }
+});
+$("button").click(function(){
+  $().css("opacity","1")
+  
+})
