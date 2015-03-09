737637ef45f2e5b212218c618595a323f838a4a6
emateo1 <emateo1@kippnyccp.org>
2015-03-09 13:19:56 -0400

    Update application.js

--- memory/application.js
+++ memory/application.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
   for(i=1; i<21; i++){
     $('#card_holder').append('<div class="card"><p>'+i+'</p></div>');
   }
+  $('.card').click(function(){
+    $(this).find('p').css("opacity", 1);
+  })
 });
-$('card').click(function(){
-  $this Find ('p').css ("opacity", 1)
-})
