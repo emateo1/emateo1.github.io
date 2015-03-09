232d726586ca2884b48d987f27b7760942999a0e
emateo1 <emateo1@kippnyccp.org>
2015-03-09 13:33:30 -0400

    Update application.js

--- memory/application.js
+++ memory/application.js
@@ -1,13 +1,25 @@
 $(document).ready(function(){
-  var my_array = ["one", "one", "two", "two", "three", "three", "four", "four", "five", "five"];
+  var my_array = ["one", "one", "two", "two", "three", "three", "four", "four", "five", "five", "six", "six", "seven", "seven", "eight", "eight", "nine", "nine","ten","ten"];
   var first_click = "a";
   var second_click = "a";
+  var click_count = 0;
   for(i in my_array){
     $('#card_holder').append('<div class="card"><p>'+my_array[i]+'</p></div>');
   }
   $('.card').click(function(){
+    if(click_count == 0){
     $(this).find('p').css("opacity", 1);
     first_click = $(this).find('p').html();
-    $(document).find("#test").append(first_click);
+    //$(document).find("#test").append(first_click);
+    }
+    else {
+    $(this).find('p').css("opacity", 1);
+    second_click = $(this).find('p').html();
+    }
+    if (first_click == second){
+      find('p').css("opacity", 1);
+    }
+    else {
+    }
   })
 });
