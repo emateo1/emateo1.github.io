bd19f634ea53a8b3622187fc4b4e10769319c8bb
emateo1 <emateo1@kippnyccp.org>
2015-05-11 13:32:00 -0400

    Create election.py

---
+++ election.py
@@ -0,0 +1,16 @@
+from random import randint
+win=0
+for i in range(10000):
+  regions = 0
+  votes =randint(1,100)
+  if <= 87:
+    regions += 1
+  votes =randint(1,100)
+  if <= 65:
+    regions += 1
+  votes =randint(1,100)
+  if <= 17:
+    regions += 1
+  if votes=2
+    win + 1
+  print win / 10000 * 100
