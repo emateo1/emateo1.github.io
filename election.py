3a922d9d0dd5d944eec1ca87b2661fd2bfc49a73
emateo1 <emateo1@kippnyccp.org>
2015-05-12 13:00:42 -0400

    Update election.py

--- election.py
+++ election.py
@@ -3,14 +3,14 @@ win=0
 for i in range(10000):
   regions = 0
   votes =randint(1,100)
-  if <= 87:
+  if <= 87:#possibility to win 
     regions += 1
   votes =randint(1,100)
-  if <= 65:
+  if <= 65:#possibility to win
     regions += 1
   votes =randint(1,100)
-  if <= 17:
+  if <= 17:#possibility to win
     regions += 1
-  if votes=2
-    win + 1
-  print win / 10000 * 100
+  if votes=>2#possibility to win
+    win += 1
+  print "the chances of winning are "+str(win/10000) # chances the candidates win in percent
